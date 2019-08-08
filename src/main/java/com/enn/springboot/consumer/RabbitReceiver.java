5b51ce56c0dc094a3c8c54c353cdcffa3f0c4c3a
Jarrettcai <380257712@qq.com>
2019-08-08 17:27:12 +0800

    4-15课程代码

--- src/main/java/com/enn/springboot/consumer/RabbitReceiver.java
+++ src/main/java/com/enn/springboot/consumer/RabbitReceiver.java
@@ -1,13 +1,17 @@
 package com.enn.springboot.consumer;
 
+import com.enn.springboot.entity.Order;
 import com.rabbitmq.client.Channel;
 
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 public class RabbitReceiver {
@@ -20,4 +24,14 @@ public class RabbitReceiver {
         Long tag = (Long) message.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
         channel.basicAck(tag,false);
     }
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "queue_2",durable = "true"),
+            exchange = @Exchange(value = "exchange_2",type = "topic"),key = "springboot.*"))
+    @RabbitHandler
+    public void onOrderMessage(@Payload Order order, Channel channel, @Headers Map<String,Object> headers) throws IOException {
+        System.err.println("--------------------------------------");
+        System.err.println("消费端Payload: " + order.toString());
+        Long tag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        channel.basicAck(tag,false);
+    }
+
 }
